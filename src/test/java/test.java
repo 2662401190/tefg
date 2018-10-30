de2d18ccf0d44c2268f8f150b22308686cd74d23
HeWei <2662401190@qq.com>
2018-10-30 14:33:08 +0800

    逆向生成表

---
+++ src/test/java/test.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author 贺威
+ * @create 2018-10-30 14:19
+ */
+public class test {
+
+
+    /**
+     * 逆向生成表
+     */
+        @Test
+        public void Generate() throws Exception{
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            File configFile = new File("src/main/resource/generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        }
+    }
+
